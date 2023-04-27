011354fbcf09f108a2b01304453500471e0f8e64
Edson Santos <edsondhonatadossantos@gmail.com>
2023-04-26 22:42:27 -0300

    Criação de controle LancheController, Criação da View Lanche. Injeção da interface Lanche no controller Lanche.

---
+++ MVC_2022/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_2022.Repositories.Interfaces;
+
+namespace MVC_2022.Controllers
+{
+    public class LancheController : Controller
+    {
+        //Injeção de dependencia da interface de lanche criada na startap.
+        private readonly ILancheRepository _lanche;
+
+        public LancheController(ILancheRepository lanche)
+        {
+            _lanche = lanche;
+        }
+        public IActionResult List()
+        {
+            //Devolvendo lista de lanche para view.
+            return View(_lanche.Lanches); ;
+        }
+    }
+}
