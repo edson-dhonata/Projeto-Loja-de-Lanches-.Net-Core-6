6a83e0e711a27300e9b80719b0cac3b4428b342f
Edson Santos <edsondhonatadossantos@gmail.com>
2023-05-12 00:33:23 -0300

    Exemplo post com list na view - Trabalho.

---
+++ MVC_2022/Controllers/TesteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_2022.ViewModels;
+
+namespace MVC_2022.Controllers
+{
+    public class TesteController : Controller
+    {
+        public IActionResult Teste()
+        {
+            return View();
+        }
+
+        public IActionResult Insert(TesteEdson meuModelo) 
+        {
+
+
+
+
+
+            return View("~/Views/Teste/Teste.cshtml",meuModelo);
+        }
+    }
+}
---
+++ MVC_2022/ViewModels/TesteEdson.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace MVC_2022.ViewModels
+{
+    public class TesteEdson
+    {
+        public TesteEdson() 
+        { 
+            this.telefones = new List<Telefone>();   
+            this.enderecos = new List<Endereco>();   
+        }
+
+        [ScaffoldColumn(false)]
+        public int codigo { get; set; }
+        [DisplayName("Nome:")]
+        public string nome { get; set; }
+        [DisplayName("Data de Nascimento:")]
+        public DateTime dataNascimento { get; set; }
+        public List<Telefone> telefones { get; set; }
+        public List<Endereco> enderecos { get; set; }
+
+    }
+
+    public class Telefone
+    {
+        public int tipo { get; set; }
+        public string numero { get; set; }
+        public string operadora { get; set; }
+    }
+
+    public class Endereco
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string bairro { get; set; }
+        public string cidade  { get; set; }
+        public string estado { get; set; }
+    }
+}
