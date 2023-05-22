5a5e2e1c0e0a1e8821f3ca1f10317c3fd7452648
Edson Santos <edsondhonatadossantos@gmail.com>
2023-05-21 22:26:24 -0300

    Criação de componente do carrinho de compras, view para mostrar itens do carrinho de compras.

--- MVC_2022/Controllers/HomeController.cs
+++ MVC_2022/Controllers/HomeController.cs
@@ -1,16 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_2022.Models;
+using MVC_2022.Repositories.Interfaces;
+using MVC_2022.ViewModels;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace MVC_2022.Controllers
 {
     //Controller principal
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _lancheRepository;
+
+        public HomeController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
         public IActionResult Index()
         {
-            TempData["obsTemp"] = "Observação TemData HomeController.";
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+
+            //TempData["obsTemp"] = "Observação TemData HomeController.";
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
