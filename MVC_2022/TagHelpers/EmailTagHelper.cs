55a8269005a0b204b28b8107a8775ec68812c801
Edson Santos <edsondhonatadossantos@gmail.com>
2023-05-25 21:19:29 -0300

    Criação de taghelper de e-mail. E página de contato.

---
+++ MVC_2022/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace MVC_2022.TagHelpers
+{
+    //Cria classe herdando de TagHelper do aspnet core.
+    public class EmailTagHelper : TagHelper
+    {
+        public string Endereco { get; set; }
+        public string Conteudo { get; set; }
+
+        //Sobreescreve método process de TagHelper do asp net core
+        //Montaremos um link para o e-mail, com as propriedades como valores.
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href","mailto:" + Endereco);  
+            output.Content.SetContent(Conteudo);
+        }
+    }
+}
